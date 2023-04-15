14c7773f94bc06a2190de1c6631ef7a39487fe16
shouryade <shourya.de12@gmail.com>
2023-04-16 03:51:53 +0530

    added login stuff and dummy route
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- routes/auth.py
+++ routes/auth.py
@@ -1,14 +1,61 @@
 from fastapi import Depends, APIRouter, HTTPException, status, Form, Request, Response
 from bleach import clean
-
-from pydantic import ValidationError
-
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from pydantic import ValidationError, BaseModel
+from typing import Annotated
 from config.db import collection
 from models.register import registerUser
-from utils import get_password_hash, get_user
+from utils import (
+    get_password_hash,
+    get_user,
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+)
 
 
 router = APIRouter(prefix="/v1/auth", tags=["authentication"])
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+@router.post("/login", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"email": user["email"]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/me/", response_model=registerUser)
+async def read_users_me(
+    current_user: Annotated[registerUser, Depends(get_current_user)]
+):
+    return current_user
+
+
+# @router.get("/users/me/items/")
+# async def read_own_items(
+#     current_user: Annotated[registerUser, Depends(get_current_user)]
+# ):
+#     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
 @router.post("/register")
